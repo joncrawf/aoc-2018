d9f7104ccd0ec0a79c45dc1a9ca4e38701b53065
Jon Crawford <jon@joncrawf.com>
2018-12-09 20:38:24 +0000

    Day 3
    Signed-off-by: Jon Crawford <jon@joncrawf.com>

---
+++ day03/2.py
@@ -0,0 +1,37 @@
+from argparse import ArgumentParser
+
+import numpy as np
+import re
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--data-path', type=str, required=True)
+    return parser
+
+
+def main():
+    args = build_parser().parse_args()
+    with open(args.data_path) as file:
+        data = file.read().splitlines()
+
+    boxes = []
+    for line in data:
+        box_id, left, top, width, height = re.search('#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)', line).groups()
+        boxes.append([int(box_id), int(left), int(top), int(width), int(height)])
+
+    furthest_right = max(boxes, key=lambda b: b[0])
+    furthest_up = max(boxes, key=lambda b: b[1])
+    env = np.zeros([furthest_up[1] + furthest_up[3] + 5, furthest_right[0] +
+                    furthest_right[2] + 5])
+
+    for box_id, left, top, width, height in boxes:
+        env[left:left+width, top:top+height] += 1
+
+    for box_id, left, top, width, height in boxes:
+        if np.all(env[left:left+width, top:top+height] == 1):
+            print(box_id)
+
+
+if __name__ == '__main__':
+    main()
